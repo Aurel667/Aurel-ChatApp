65b46339e9ccc14509ec5793bb691f2d5caa1879
Aurel KOLANI <aureliuskolani@gmail.com>
2025-09-09 00:54:44 +0000

    features: chat, notifications, friends

--- frontend/src/components/MessageItem.jsx
+++ frontend/src/components/MessageItem.jsx
@@ -11,7 +11,7 @@ export default function MessageItem({message, index, user, messages}){
             {(index == previousIndex) && (date == previousDate) && <p className="mx-auto text-sm text-white font-semibold py-1 text-center my-2 rounded-full bg-neutral-700/80 px-2"> {date == new Date().toLocaleDateString('fr-FR') ? "Aujourd'hui" : date} </p>}
             {(index != previousIndex) && (date != previousDate) && <p className="mx-auto text-sm text-white font-semibold py-1 text-center my-2 rounded-full bg-neutral-700/80 px-2"> {date == new Date().toLocaleDateString('fr-FR') ? "Aujourd'hui" : date} </p>}
             <div className={`flex my-1 ${isMine ? "justify-end" : "justify-start"}`}>
-                <div className={` flex gap-2 items-center p-2 ${isMine ? "bg-fuchsia-800/80 rounded-tl-md animate-from-right-to-left" : "bg-neutral-700 rounded-tr-md animate-from-left-to-right"} rounded-b-md text-white`}>
+                <div className={` flex gap-2 items-center px-2 ${isMine ? "bg-fuchsia-800/80 rounded-tl-md animate-from-right-to-left" : "bg-neutral-700 rounded-tr-md animate-from-left-to-right"} rounded-b-md text-white`}>
                     <p className="text-white text-sm"> {message?.text} </p>
                     <div className="flex items-center mt-5 gap-2">
                         <p className="text-gray-200 text-end text-[8pt]"> {new Date(message?.createdAt).getHours()}:{new Date(message?.createdAt).getMinutes()} </p>
